b800dbf361d53a07b3c2bbcd693e73ca99abaf4f
ptrodion <rdnpetrenko@gmail.com>
2023-09-26 20:45:24 +0100

    https

--- src/components/MoviesDetails/MoviesDetails.jsx
+++ src/components/MoviesDetails/MoviesDetails.jsx
@@ -26,7 +26,9 @@ export const MoviesDetails = ({
       <MovDetTitleWrapper>
         <img
           src={
-            image ? `https:/image/tmd/b/org/t/p/original${image}` : defaultImg
+            image
+              ? `// https://image.tmdb.org/t/p/original${image}`
+              : defaultImg
           }
           alt={title}
           width={150}
